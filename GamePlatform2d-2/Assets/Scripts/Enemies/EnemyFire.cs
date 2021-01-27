7bca85e5e5b764e546f5137f0055098ab1f9df9b
Giovanni Maciel <giomaciel1@gmail.com>
2021-01-27 00:42:58 -0400

    Added enemies animations and attack mechanics
    Adição das primeiras mecanicas do primeiro inimigo, adicionados também as animações de parado, atacando e morrendo.

---
+++ GamePlatform2d-2/Assets/Scripts/Enemies/EnemyFire.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+{
+    public float fireRate;
+
+    public Rigidbody2D bulletPrefab;
+    public Transform shotSpawn;
+    public Vector2 shotImpulse;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SetFire", fireRate, fireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetFire()
+    {
+        anim.SetTrigger("Fire");
+    }
+
+    private void Fire()
+    {
+        Rigidbody2D newBullet = Instantiate(bulletPrefab, shotSpawn.position, transform.rotation);
+        newBullet.AddForce(shotImpulse, ForceMode2D.Impulse);
+        Destroy(newBullet.gameObject, 5);
+    }
+}
