f7c14432fbb6145cc043214c866876bb2de2bace
Giovanni Maciel <giomaciel1@gmail.com>
2021-02-04 13:29:37 -0400

    Added enemies animations and attack mechanics II
    Foram adicionados novos inimigos e novas mecanicas, como a de fazer o inimigo andar e atacar, em relação ao atacar, já está interagindo com o player.

---
+++ GamePlatform2d-2/Assets/Scripts/Enemies/EnemyChomper.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChomper : MonoBehaviour
+{
+    [Header("Movimentação")]
+    public float speed;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    [Header("Flip By Time")]
+    public bool flipByTime;
+    public float flipTime = 3f;
+
+    [Header("Ataque")]
+    public float attackRate;
+    public float attackDistance;
+    private Transform player;
+    private float nextAttack;
+
+    private Rigidbody2D rb;
+    private bool onGround;
+    private Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        if(flipByTime)
+        {
+            InvokeRepeating("Flip", flipTime, flipTime);
+        }
+    }
+
+    void Update()
+    {
+        if(flipByTime)
+        {
+            return;
+        }
+
+        onGround = Physics2D.Linecast(transform.position, groundCheck.position, groundLayer);
+
+        if(!onGround)
+        {
+            Flip();
+        }
+
+        CheckTarget();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * speed;
+    }
+
+    void CheckTarget()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+        float dir = player.transform.position.x - transform.position.x;
+
+        if(distance < attackDistance)
+        {
+            if((speed < 0 && dir < 0) || (speed > 0 && dir > 0))
+            {
+                if(Time.time > nextAttack)
+                {
+                    nextAttack = Time.time + attackRate;
+                    anim.SetTrigger("Attack");
+                }
+            }
+        }
+    }
+
+    void Flip()
+    {
+        speed *= -1;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
