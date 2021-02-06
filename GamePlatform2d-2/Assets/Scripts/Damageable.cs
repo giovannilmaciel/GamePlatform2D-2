5d92d0f4a159d8fbede508a5e9c2207705545f48
Giovanni Maciel <giomaciel1@gmail.com>
2021-02-05 22:59:47 -0400

    Added a script to take damage, for the player and enemies
    Adicionado um script de receber dano, tanto para o player quanto para os inimigos.

---
+++ GamePlatform2d-2/Assets/Scripts/Damageable.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour
+{
+    public UnityEvent OnDamage;
+    public UnityEvent ReleaseDamage;
+    public UnityEvent OnDeath;
+
+    public int maxHealth;
+    public float invincibleTime;
+
+    private int currentHealth;
+    private bool invincible;
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if(invincible || isDead)
+        {
+            return;
+        }
+
+        OnDamage.Invoke();
+        invincible = true;
+        Invoke("SetInvincible", invincibleTime);
+        currentHealth -= damageAmount;
+
+        if(currentHealth <= 0)
+        {
+            isDead = true;
+            OnDeath.Invoke();
+        }
+    }
+
+    void SetInvincible()
+    {
+        invincible = false;
+    }
+}
