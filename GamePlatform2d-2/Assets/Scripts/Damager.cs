faa1c2585df5ccd6bff665d12e3a9566053095a6
Giovanni Maciel <giomaciel1@gmail.com>
2021-02-08 15:19:48 -0400

    Created a camera shaker
    Adicionada uma tremida na camera sempre que o player dá um dano ou recebe um dano de um inimigo.

--- GamePlatform2d-2/Assets/Scripts/Damager.cs
+++ GamePlatform2d-2/Assets/Scripts/Damager.cs
@@ -6,12 +6,17 @@ public class Damager : MonoBehaviour
 {
     public int power = 1;
 
+    [Header("Shake Values")]
+    public float powerValue = 50;
+    public float duration = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Damageable damageable = other.GetComponent<Damageable>();
         if(damageable != null)
         {
             damageable.TakeDamage(power, transform.position.x);
+            Shaker.instance.SetValues(powerValue, duration);
         }
     }
 }
