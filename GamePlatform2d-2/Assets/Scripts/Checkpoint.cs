605f404818c002d449c2405ba2fd338db6a916e4
Giovanni Maciel <giomaciel1@gmail.com>
2021-02-09 22:12:54 -0400

    Added checkpoint mechanics
    Adicionada a primeira mecanica de checkpoint, nessa primeira parte foi adicionado o sprite do checkpoint, sendo que assim que o player passar pelo sprite, ele acenderá.

---
+++ GamePlatform2d-2/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Sprite checkPointLighted;
+    public GameObject lights;
+
+    private bool isActive;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isActive)
+        {
+            return;
+        }
+
+        if(other.CompareTag("Player"))
+        {
+            spriteRenderer.sprite = checkPointLighted;
+            lights.SetActive(true);
+        }
+    }
+}
